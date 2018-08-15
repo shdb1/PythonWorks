26d10cb539c80ee18a5acff4be2123d72214c56f
Mohammad Shadab <shadab2266@gmail.com>
2018-08-15 21:34:37 +0530

    pre order traversal

---
+++ com/shadab/python/challenges/tree/PreOrderTraversal.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+                    
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+preOrder(tree.root)
+
+     """
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    if root is not None:
+        print(root.info, end=' ', flush=True);
+        if root.left is not None:
+            preOrder(root.left);
+        if root.right is not None:
+            preOrder(root.right);
+            
+    else:
+        print('not a valid tree')
+        
+        
+ 
+
